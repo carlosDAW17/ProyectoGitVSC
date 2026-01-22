c92c7a60b4c646f362d258975f03fa6823e65ff7
carlosDAW17 <carlosmarcodaza@gmail.com>
2026-01-22 12:05:18 +0100

    Añadido archivo Saludo.java con mensajes de saludo y despedida

---
+++ src/src/Saludo.java
@@ -0,0 +1,10 @@
+public class Saludo {
+
+    public static void saludoInicial() {
+        System.out.println("¡Hola! Bienvenido al proyecto.");
+    }
+
+    public static void despedida() {
+        System.out.println("¡Hasta luego! Gracias por usar el programa.");
+    }
+}
